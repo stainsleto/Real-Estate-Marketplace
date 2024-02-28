4d5cf1e45adf925899044994ae25b4356f557694
stainsleto <stainsmault@gmail.com>
2024-02-28 21:14:45 +0530

    @stainsleto/ initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-3xl font-bold'>
+        RealEstate Application
+      </div>
+    </>
+  )
+}
+
+export default App
