b5010c1b193b65b254f9441f7acc93f8ada3c617
Sanjay <sanjay@Sanjays-MacBook-Air.local>
2024-03-13 11:46:43 +0530

    @sanjay929 added cors for updates

--- api/index.js
+++ api/index.js
@@ -3,14 +3,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const adminRouter = require('./routes/admin')
 const userRouter = require('./routes/user')
+const cors = require('cors')
 
 
-
+app.use(cors())
 
 app.use(bodyParser.json())
 
 app.use('/admin',adminRouter)
 app.use('/user',userRouter)
+
 
 const PORT = 3000
 
